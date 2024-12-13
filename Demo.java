1da12b7440534765f682c0047f7e2e6baf2e7fba
Raushan Kumar <raushan.kumar@synlabs.io>
2024-12-13 15:27:31 +0530

    commit

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hello from jenkins");
+        System.out.println("hello from spring boot");
+
     }
 }
